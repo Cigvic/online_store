8e64c7cf65d3c6ebcf9b947290e2891d0075760d
Цигвинцев Илья <tsigvintsev@edu.surgu.ru>
2022-07-11 17:05:42 +0500

    add userService, deviceService

--- server/routes/device/deviceRouter.js
+++ server/routes/device/deviceRouter.js
@@ -5,7 +5,8 @@ const deviceController = require('./deviceController')
 router.post('/', deviceController.create)
 router.get('/', deviceController.getAll)
 router.delete('/', deviceController.deleteOne)
-router.get('/one', deviceController.getOne)
+router.get('/one/:id', deviceController.getOne)
 router.get('/new', deviceController.new)
+router.post('/rating', deviceController.rating)
 
 module.exports = router
